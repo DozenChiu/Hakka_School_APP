0fe38cf5183b66704adcd58428ca48654326533a
OddHan123 <hank01261224@gmail.com>
2024-08-20 15:37:34 +0800

    新增響應式 UI

--- lib/Pages/learning_progress_page.dart
+++ lib/Pages/learning_progress_page.dart
@@ -9,87 +9,103 @@ class LearningProgressPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-          title: const Text(
-        '答題進度',
-        style: TextStyle(color: Colors.white),
-      )),
-      body: Center(
-        child: FutureBuilder<List<Progress>?>(
-          future: dbHelper.getQuestionCount(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const CircularProgressIndicator();
-            } else if (snapshot.hasError) {
-              return Text('Error: ${snapshot.error}');
-            } else if (!snapshot.hasData) {
-              return const Text('No data found');
-            } else {
-              final list = snapshot.data!;
-              List<double> result = [0,0,0]; // 計算每種測驗的進度
-              for (int i=0; i<3; i++) {
-                result[i] = list[i].correct / list[1].ttl;
-              }
-              return Column(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                      margin: const EdgeInsets.all(16.0),
-                      width: 150,
-                      height: 35,
-                      child: Column(
-                          children: [
-                            Text('單句測驗：${(result[0]*100).toStringAsFixed(2)}%'),
-                            LinearProgressIndicator(
-                              value: result[0],
-                              backgroundColor: Colors.grey.shade200,
-                              valueColor: const AlwaysStoppedAnimation<Color>(Colors.green),
-                              minHeight: 15.0,
-                            ),
-                          ]
-                      )
-                  ),
-                  Container(
-                      margin: const EdgeInsets.all(16.0),
-                      width: 150,
-                      height: 35,
-                      child: Column(
-                          children: [
-                            Text('對話理解：${(result[1]*100).toStringAsFixed(2)}%'),
-                            LinearProgressIndicator(
-                              value: result[1],
-                              backgroundColor: Colors.grey.shade200,
-                              valueColor: const AlwaysStoppedAnimation<Color>(Colors.green),
-                              minHeight: 15.0,
-                            ),
-                          ]
-                      )
-                  ),
-                  Container(
-                      margin: const EdgeInsets.all(16.0),
-                      width: 150,
-                      height: 35,
-                      child: Column(
-                          children: [
-                            Text('閱讀測驗：${(result[2]*100).toStringAsFixed(2)}%'),
-                            LinearProgressIndicator(
-                              value: result[2],
-                              backgroundColor: Colors.grey.shade200,
-                              valueColor: const AlwaysStoppedAnimation<Color>(Colors.green),
-                              minHeight: 15.0,
-                            ),
-                          ]
-                      )
-                  ),
-                ],
-              );
-            }
-          },
-        ),
-      ),
-      bottomNavigationBar: const BottomNavBar(selectedIndex: 3), // 底部導航欄
+    return LayoutBuilder(
+        builder: (context, constraints) {
+          double fontSize = ((constraints.maxWidth/30)<24)?24:(constraints.maxWidth/30); // 設定字體根據寬度調整，保留最小大小
+          double paddingHeight = constraints.maxHeight/20; // 設定 SizedBox 根據高度調整
+          return Scaffold(
+            appBar: AppBar(
+                title: const Text(
+                  '答題進度',
+                  style: TextStyle(color: Colors.white),
+                )),
+            body: Center(
+              child: FutureBuilder<List<Progress>?>(
+                future: dbHelper.getQuestionCount(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const CircularProgressIndicator();
+                  } else if (snapshot.hasError) {
+                    return Text('Error: ${snapshot.error}');
+                  } else if (!snapshot.hasData) {
+                    return const Text('No data found');
+                  } else {
+                    final list = snapshot.data!;
+                    List<double> result = [0,0,0]; // 計算每種測驗的進度
+                    for (int i=0; i<3; i++) {
+                      result[i] = list[i].correct / list[1].ttl;
+                    }
+                    return Column(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                            margin: const EdgeInsets.all(16.0),
+                            width: constraints.maxWidth*(1/3),
+                            height: constraints.maxHeight/6,
+                            child: Column(
+                                children: [
+                                  Text('單句測驗：${(result[0]*100).toStringAsFixed(2)}%',
+                                    style: TextStyle(fontSize: fontSize*0.6, fontWeight: FontWeight.bold),
+                                  ),
+                                  SizedBox(height: paddingHeight*0.5),
+                                  LinearProgressIndicator(
+                                    value: result[0],
+                                    backgroundColor: Colors.grey.shade200,
+                                    valueColor: const AlwaysStoppedAnimation<Color>(Colors.green),
+                                    minHeight: paddingHeight*0.5,
+                                  ),
+                                ]
+                            )
+                        ),
+                        Container(
+                            margin: const EdgeInsets.all(16.0),
+                            width: constraints.maxWidth*(1/3),
+                            height: constraints.maxHeight/6,
+                            child: Column(
+                                children: [
+                                  Text('對話理解：${(result[1]*100).toStringAsFixed(2)}%',
+                                    style: TextStyle(fontSize: fontSize*0.6, fontWeight: FontWeight.bold),
+                                  ),
+                                  SizedBox(height: paddingHeight*0.5),
+                                  LinearProgressIndicator(
+                                    value: result[1],
+                                    backgroundColor: Colors.grey.shade200,
+                                    valueColor: const AlwaysStoppedAnimation<Color>(Colors.green),
+                                    minHeight: paddingHeight*0.5,
+                                  ),
+                                ]
+                            )
+                        ),
+                        Container(
+                            margin: const EdgeInsets.all(16.0),
+                            width: constraints.maxWidth*(1/3),
+                            height: constraints.maxHeight/6,
+                            child: Column(
+                                children: [
+                                  Text('閱讀測驗：${(result[2]*100).toStringAsFixed(2)}%',
+                                    style: TextStyle(fontSize: fontSize*0.6, fontWeight: FontWeight.bold),
+                                  ),
+                                  SizedBox(height: paddingHeight*0.5),
+                                  LinearProgressIndicator(
+                                    value: result[2],
+                                    backgroundColor: Colors.grey.shade200,
+                                    valueColor: const AlwaysStoppedAnimation<Color>(Colors.green),
+                                    minHeight: paddingHeight*0.5,
+                                  ),
+                                ]
+                            )
+                        ),
+                      ],
+                    );
+                  }
+                },
+              ),
+            ),
+            bottomNavigationBar: const BottomNavBar(selectedIndex: 3), // 底部導航欄
+          );
+        }
     );
+
   }
 }
--- lib/Pages/question_bank_page.dart
+++ lib/Pages/question_bank_page.dart
@@ -9,75 +9,87 @@ class QuestionBankPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-      return Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            '題庫',
-            style: TextStyle(color: Colors.white), // 設置標題文字顏色為白色
-          ),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const Text(
-                  '聽力能力',
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                ),
-                const SizedBox(height: 20), // 添加一個高度為20的空間
-                ElevatedButton.icon(
-                  icon: const Icon(Icons.hearing), // 設置按鈕左側的圖標
-                  label: const Text('單句'), // 設置按鈕文字
-                  onPressed: () {
-                    // 當按鈕被按下時導航到單句頁面
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const SingleSentencePage()),
-                    );
-                  },
-                ),
-                const SizedBox(height: 10), // 添加一個高度為10的空間
-                ElevatedButton.icon(
-                  icon: const Icon(Icons.hearing), // 設置按鈕左側的圖標
-                  label: const Text('對話'), // 設置按鈕文字
-                  onPressed: () {
-                    // 當按鈕被按下時導航到對話頁面
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) =>
-                          const DialoguePage()), // 創建導航到 DialoguePage 頁面
-                    );
-                  },
-                ),
-
-                const SizedBox(height: 40), // 添加一個高度為40的空間，分隔不同類別
-                const Text(
-                  '閱讀能力',
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                ),
-
-                const SizedBox(height: 20), // 添加一個高度為20的空間
-                ElevatedButton.icon(
-                  icon: const Icon(Icons.book), // 設置按鈕左側的圖標
-                  label: const Text('閱測'), // 設置按鈕文字
-                  onPressed: () {
-                    // 當按鈕被按下時導航到閱測頁面
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const ReadingTestPage()),
-                    );
-                  },
-                ),
-              ],
+    return LayoutBuilder(
+        builder: (context, constraints) {
+          double fontSize = ((constraints.maxWidth/30)<24)?24:(constraints.maxWidth/30); // 設定字體根據寬度調整，保留最小大小
+          double paddingHeight = constraints.maxHeight/20; // 設定 SizedBox 根據高度調整
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text(
+                '題庫',
+                style: TextStyle(color: Colors.white), // 設置標題文字顏色為白色
+              ),
             ),
-          ),
-        ),
-        bottomNavigationBar: const BottomNavBar(selectedIndex: 1), // 底部導航欄
+            body: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      '聽力能力',
+                      style: TextStyle(fontSize: fontSize, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: paddingHeight), // 添加一個高度為20的空間
+                    ElevatedButton.icon(
+                      icon: Icon(Icons.hearing, size: fontSize*0.6,), // 設置按鈕左側的圖標
+                      label: Text('單句', // 設置按鈕文字
+                        style: TextStyle(fontSize: fontSize*0.6)
+                      ),
+                      onPressed: () {
+                        // 當按鈕被按下時導航到單句頁面
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const SingleSentencePage()),
+                        );
+                      },
+                    ),
+                    SizedBox(height: paddingHeight), // 添加一個高度為10的空間
+                    ElevatedButton.icon(
+                      icon: Icon(Icons.hearing, size: fontSize*0.6,), // 設置按鈕左側的圖標
+                      label: Text('對話',
+                        style: TextStyle(fontSize: fontSize*0.6),
+                      ), // 設置按鈕文字
+                      onPressed: () {
+                        // 當按鈕被按下時導航到對話頁面
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) =>
+                              const DialoguePage()), // 創建導航到 DialoguePage 頁面
+                        );
+                      },
+                    ),
+
+                    SizedBox(height: paddingHeight), // 添加一個高度為40的空間，分隔不同類別
+                    Text(
+                      '閱讀能力',
+                      style: TextStyle(fontSize: fontSize, fontWeight: FontWeight.bold),
+                    ),
+
+                    SizedBox(height: paddingHeight), // 添加一個高度為20的空間
+                    ElevatedButton.icon(
+                      icon: Icon(Icons.book, size: fontSize*0.6,), // 設置按鈕左側的圖標
+                      label: Text('閱測',
+                        style: TextStyle( fontSize: fontSize*0.6),
+                      ), // 設置按鈕文字
+                      onPressed: () {
+                        // 當按鈕被按下時導航到閱測頁面
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const ReadingTestPage()),
+                        );
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            bottomNavigationBar: const BottomNavBar(selectedIndex: 1), // 底部導航欄
+          );
+        }
     );
   }
 }
